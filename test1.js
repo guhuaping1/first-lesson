978f05be29c548de5692287a8add7ee501e91158
guhuaping1 <adsswox@163.com>
2017-06-30 17:17:42 +0800

    first complete
    v01.01

---
+++ test1.js
@@ -0,0 +1,83 @@
+Package Control Messages
+========================
+
+Emmet
+-----
+
+  Thank you for installing Emmet -- a toolkit that can greatly improve your workflow. Note that this plugin automatically downloads and installs PyV8 binary (see status bar message). 
+  
+  ******************************
+  Please restart editor 
+  to finish installation process 
+  after PyV8 was downloaded.
+  ******************************
+  
+  Tab key handler
+  ==========================
+  
+  By default, Emmet allows you to expand abbreviations with Tab key in HTML, XML, HAML and CSS/SASS/LESS/Stylus documents. As a side effect, you can’t use some of your ST2 snippets.
+  
+  Please read https://github.com/sergeche/emmet-sublime#tab-key-handler about how Tab handler works and how to tweak its behavior to match your needs.
+  
+  Enter key
+  ==========================
+  
+  In HTML and XML documents, Emmet overrides Enter key to insert formatted line breaks between opening and closing tags. In some cases it will break character input (for example, in Japanese language).
+  
+  To disable Enter key handler, simply add the following option in your user's Preferences file:
+  
+  "disable_formatted_linebreak": true
+  
+  Actions shortcuts
+  ==========================
+  
+  Emmet has a number of actions with keyboard shortcuts that may override ones you're using commonly (for example, Ctrl-E or Ctrl-Down). Please read the project main page to get list of available actions and keyboard shortcuts and how to disable them:
+  https://github.com/sergeche/emmet-sublime
+  
+  Documentation and examples:
+  http://emmet.io
+  
+  ------------------------------
+  Follow me on Twitter: @emmetio
+  ------------------------------
+
+
+TrailingSpaces
+--------------
+
+  
+  
+      Thank you for installing Trailing Spaces
+      ----------------------------------------
+  
+      You're now ready to give trailing spaces *a hard time*!
+  
+  
+  Documentation
+  =============
+  
+  Although the usage of this plugin is dead simple, it comes with several options. All
+  details are available in the documentation, and you can read it by clicking on
+  "Preferences / Package Settings / Trailing Spaces / Help", or in a prettier form, by
+  browsing https://github.com/SublimeText/TrailingSpaces.
+  
+  Key Binding
+  ===========
+  
+  This plugin does not come with a default key binding for the deletion command. You can
+  pick your own key binding and define it in "Preferences / Key Bindings - User", or just
+  stick to using the menu entry under "Edit". Check the help for advice on this.
+  
+  Upgrades & Issues
+  =================
+  
+  Package Control will automatically update all packages every time the editor is started,
+  so there is nothing for you to worry about. If you however do find the plugin not to work
+  as it used to, head to the issues tracker (see links below) to report the problem.
+  
+  Useful Links
+  ============
+  
+  * Documentation & Code: https://github.com/SublimeText/TrailingSpaces
+  * Report issues / Request New Features / Roadmap: https://github.com/SublimeText/TrailingSpaces/issues
+  * Follow me on twitter: @jdvauguet
